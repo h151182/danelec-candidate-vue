c9d74534af0dd97bfb82994debe6a8902642c273
h151182 <randimariesev@hotmail.com>
2023-06-19 20:00:51 +0200

    Update YouTubeSearch.vue
    Updated typo in toolbar title

--- src/views/YouTubeSearch.vue
+++ src/views/YouTubeSearch.vue
@@ -2,7 +2,10 @@
   <div>
     <v-card class="elevation-12">
       <v-toolbar color="primary" dark flat>
-        <v-toolbar-title>YoutTube Search</v-toolbar-title>
+         <!--
+  replaced typo YoutToube with YouTube
+  -->
+        <v-toolbar-title>YouTube Search</v-toolbar-title>
       </v-toolbar>
       <v-card-text>
         <v-text-field
