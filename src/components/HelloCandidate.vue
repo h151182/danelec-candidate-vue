e90db342d5926770c55fd3cf172ce9f42135be75
h151182 <randimariesev@hotmail.com>
2023-06-21 13:45:08 +0200

    Update HelloCandidate.vue
    Updated text and emotion

--- src/components/HelloCandidate.vue
+++ src/components/HelloCandidate.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    
     <v-col>
       <v-sheet class="elevation-12 pa-2">
         <v-img src="../assets/logo.svg"></v-img>
@@ -7,12 +8,12 @@
     </v-col>
     <v-col>
       <v-card class="elevation-12">
-        <v-card-title> Great! You got it working... </v-card-title>
+        <v-card-title> Great! It's working </v-card-title>
 
         <v-card-text>
           <v-alert type="warning" border="left" elevation="2" colored-border>
-            ...but looks like our YouTube Search is down
-            <v-icon>mdi-emoticon-cry-outline</v-icon><br />
+            Our YouTube Search is up
+            <v-icon>mdi-emoticon-cool-outline</v-icon><br />
           </v-alert>
         </v-card-text>
       </v-card>
